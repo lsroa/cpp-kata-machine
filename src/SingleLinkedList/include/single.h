b1d69414982eb8d0cd98461eda8bbaf915bc741b
Luis Roa <luis.roa@mac.local>
2024-02-12 20:57:08 +0100

    Linked list initial implementation

---
+++ src/SingleLinkedList/include/single.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <memory>
+
+template <typename T>
+class Node {
+ public:
+  Node(T data) : data(data){};
+  std::shared_ptr<Node> next;
+  T data;
+};
+
+template <typename T>
+class LinkedList {
+ public:
+  LinkedList() = default;
+  std::shared_ptr<Node<T>> head;
+  std::shared_ptr<Node<T>> tail;
+  int length;
+  void append(T data);
+  void prepend(T data);
+  T get(int index);
+  T remove(T data);
+  T removeAt(int index);
+};
+
+template <typename T>
+T LinkedList<T>::get(int index) {
+  auto node = this->head;
+
+  if (index > this->length) {
+    return T();
+  }
+
+  for (int i = 0; i < index; i++) {
+    node = node->next;
+  }
+
+  return node->data;
+};
+
+template <typename T>
+T LinkedList<T>::removeAt(int index) {
+  auto node = this->head;
+  std::shared_ptr<Node<T>> prev;
+
+  for (int i = 0; i < index; i++) {
+    prev = node;
+    node = node->next;
+  }
+
+  prev->next = node->next;
+  T value = node->data;
+
+  this->length--;
+  return value;
+};
+
+template <typename T>
+void LinkedList<T>::prepend(T data) {
+  auto new_node = std::make_shared<Node<T>>(data);
+  new_node->next = this->head;
+  this->head = new_node;
+  this->length++;
+};
+
+template <typename T>
+void LinkedList<T>::append(T data) {
+  auto new_node = std::make_shared<Node<T>>(data);
+  if (this->length == 0) {
+    this->head = this->tail = new_node;
+    this->length++;
+    return;
+  }
+  this->tail->next = new_node;
+  this->tail = new_node;
+  this->length++;
+};
---
+++ src/SingleLinkedList/test/test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include "single.h"
+
+TEST(SingleLinkedList, List) {
+  auto list = new LinkedList<int>();
+
+  list->append(5);
+  list->append(7);
+  list->append(9);
+
+  EXPECT_EQ(list->get(2), 9);
+  EXPECT_EQ(list->removeAt(1), 7);
+  EXPECT_EQ(list->length, 2);
+
+  list->append(11);
+  /* expect(list.removeAt(1)).toEqual(9); */
+  /* expect(list.remove(9)).toEqual(undefined); */
+  /* expect(list.removeAt(0)).toEqual(5); */
+  /* expect(list.removeAt(0)).toEqual(11); */
+  /* expect(list.length).toEqual(0); */
+
+  /* list.prepend(5); */
+  /* list.prepend(7); */
+  /* list.prepend(9); */
+
+  /* expect(list.get(2)).toEqual(5); */
+  /* expect(list.get(0)).toEqual(9); */
+  /* expect(list.remove(9)).toEqual(9); */
+  /* expect(list.length).toEqual(2); */
+  /* expect(list.get(0)).toEqual(7); */
+}
